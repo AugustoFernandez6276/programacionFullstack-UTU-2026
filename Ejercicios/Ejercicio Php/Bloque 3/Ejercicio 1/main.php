93b8ae512aaef3e7e1842d1a45fe22a325343dfd
AugustoFernandez6276 <augustofernandez6276@gmail.com>
2026-07-24 20:35:44 -0300

    mai

--- Ejercicios/Ejercicio Php/Bloque 3/Ejercicio 1/main.php
+++ Ejercicios/Ejercicio Php/Bloque 3/Ejercicio 1/main.php
@@ -3,10 +3,13 @@
 $numero1 = 1;
 $numero2 = 2;
 
-if ($numero1 > $numero2) {
-    echo "El numero 1 es mayor que el numero 2";
+if ($numero1 == $numero2) {
+    echo "Son iguales ";
+
+    } else if ($numero1 > $numero2) {
+    echo "El numero 1 es mayor";
 } else {
-    echo "El numero 2 es mayor que el numero 1";
+    echo "El numero 2 es mayor";
 }
 
 ?>
